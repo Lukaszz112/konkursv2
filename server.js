2a4a356f94346b8ef3b156e38670c91e4e4d65f7
Lukaszz112 <93291505+Lukaszz112@users.noreply.github.com>
2021-12-13 19:23:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,27 +9,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.use(express.urlencoded({ extended: false }))
 
-
-
-app.get('/filter', (req, res) => {
-  res.render('filter.ejs')
-})
-
-app.get('/collect', (req, res) => {
-  res.render('collect.ejs')
-})
-
-app.get('/about', (req, res) => {
-  res.render('about.ejs')
-})
-
-app.get('/recycling', (req, res) => {
-  res.render('recycling.ejs')
-})
-
-
 let TrashList = [];
-
 
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://ReCycleMan:zaq1%40WSX@recycle.lonnt.mongodb.net/TrashList?retryWrites=true&w=majority";
@@ -83,96 +63,6 @@ async function run() {
 
 run().catch(console.dir);
 
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-//     // Establish and verify connection
-//     await client.db("admin").command({ ping: 1 }); 
-//     console.log("Connected successfully to server");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-
-app.get('/trashList', (req, res) => {
-        
-TrashList = []
-  
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://ReCycleMan:zaq1%40WSX@recycle.lonnt.mongodb.net/TrashList?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-async function run() {
-
-  try {
-
-    await client.connect();
-
-    const database = client.db("TrashList");
-
-    const trash = database.collection("ReCycleBase");
-
-    // query for movies that have a runtime less than 15 minutes
-
-    const query = {};
-
-    const options = {
-      
-      
-
-      projection: { _id: 0, Item: 1, trashType: 1 },
-
-    };
-
-    const cursor = trash.find(query, options);
-
-    // print a message if no documents were found
-
-    if ((await cursor.count()) === 0) { 
-
-      console.log("No documents found!");
-
-    }
-
-    // replace console.dir with your callback to access individual elements
-    TrashList = [];
-    // await cursor.forEach(console.dir);
-    await cursor.forEach(function listItems(x){
-
-      TrashList.push(x)
-    })
-     
-
-  } finally {
-    
-
-    await client.close();
-    
-  }
-  
- 
-
-}
-
-async function showList(){
- 
-try {  
- await run().catch();
-} finally { 
-  res.render('trash.ejs', {uri: uri, client: client, TrashList: TrashList, run: run()})
-}
-}
-  
-  showList();
-  
-}
-)
-
 var axios = require('axios');
 var data = JSON.stringify({
     "collection": "ReCycleBase",
@@ -202,5 +92,10 @@ axios(config)
         console.log(error);
     });
 
+
+
+app.get('/', function(req,res){
+    res.send(JSON.stringify(config.data))
+});  
 
 app.listen(3300)
